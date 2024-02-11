f82ea22f9c1c4553657a245bb0540347e6969b48
QAAutomation-lab <automationguide13@gmail.com>
2024-02-11 08:35:09 +0530

    API Framework

---
+++ APISessionsWithFramework/src/main/java/api/endpoints/BaseURLs.java
@@ -0,0 +1,29 @@
+package api.endpoints;
+
+public class BaseURLs {
+	//Base URL
+	public static final String base_url="https://petstore.swagger.io/v2";
+	
+	//Flow: User--> preparing final URL's for scripting
+	public static final String post_url=base_url+"/user";
+	public static String get_url=base_url+"/user/{username}";
+	public static String update_url=base_url+"/user/{username}";
+	public static String delete_url=base_url+"/user/{username}";
+	
+	//Flow: Store
+	
+	//Flow: Pet
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
